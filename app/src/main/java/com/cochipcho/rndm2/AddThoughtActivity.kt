6b405ab90a8b97795db3b98309a79bfe8183e94c
Myung Hoon So <joa214@hotmail.com>
2020-06-19 13:29:27 +0100

    initial commit

---
+++ app/src/main/java/com/cochipcho/rndm2/AddThoughtActivity.kt
@@ -0,0 +1,12 @@
+package com.cochipcho.rndm2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddThoughtActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_thought)
+    }
+}
---
+++ app/src/main/java/com/cochipcho/rndm2/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.cochipcho.rndm2
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            val addThoughtIntent = Intent(this, AddThoughtActivity::class.java)
+            startActivity(addThoughtIntent)
+
+        }
+    }
+
+}
